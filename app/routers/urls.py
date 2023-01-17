2c3ca8d53054dca6d084503c5c88dc3294dceb42
Ivan Agosto <spartan360@live.com.mx>
2023-01-17 14:00:13 -0600

    feat: Route endpoints updated

--- app/routers/urls.py
+++ app/routers/urls.py
@@ -1,13 +1,21 @@
 from fastapi import APIRouter, Depends
+from services import urls
 from models.url import UrlInput
 from database import get_db
 
 router = APIRouter()
 
+@router.get('/')
+async def get_index(db = Depends(get_db)):
+    return { 'url': 'asd' }
+
+
 @router.get('/{code}')
-async def getUrl(code: str, db = Depends(get_db)):
-    return { 'url': 'asd', 'code': code }
+async def get_url(code: str, db = Depends(get_db)):
+    url = urls.get(db, code)
+    return { 'url': url.original_url }
 
 @router.post('/')
-async def createUrl(body: UrlInput, db = Depends(get_db)):
-    return { 'url': body.url }
+async def create_url(body: UrlInput, db = Depends(get_db)):
+    short_url = urls.create(db, body.url)
+    return { 'short_url': short_url }
